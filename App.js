926ac05222de42f793b3e0b5f321b7a8940aace2
sam2002jain <jainsam1975@gmail.com>
2024-02-17 13:08:22 +0530

    login UI done

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log('Logging in with:', email, password);
+  };
+
+  const handleForgotPassword = () => {
+    // Implement your forgot password logic here
+    console.log('Forgot password');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('/Users/a/ContainerTrackingApp/assets/bg.png')} style={styles.background}>
+      <View style={styles.container}>
+        <Image source={require('/Users/a/ContainerTrackingApp/assets/logo.png')} style={styles.logo} />
+        <Text style={styles.title}>Seabird CFS Container Tracking</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={setEmail}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleForgotPassword}>
+          <Text style={styles.forgotPassword}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
+    borderRadius:10 // You can add a background color to input fields if needed
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#40A2D8',
+    padding: 10,
+    borderRadius: 10,
+  },
+  forgotPassword: {
+    marginTop: 10,
+    color: 'blue',
+    textDecorationLine: 'underline',
   },
 });
